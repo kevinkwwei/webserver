36b8dfaf5eca7bbdcb24d45b000a1038b26acba3
kevinkwwei <kevinkwwei@tencent.com>
2023-08-01 23:52:29 +0800

    update files

---
+++ httpproject/src/httpclient/http_client.h
@@ -0,0 +1,13 @@
+//
+// Created by Kevin Wei on 2023/8/1.
+//
+
+#pragma once
+
+#include <memory>
+
+
+class HttpClient : std::enable_shared_from_this<HttpClient> {
+
+};
+
---
+++ httpproject/src/httpserver/http_server.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Kevin Wei on 2023/8/1.
+//
+
+#include "http_server.h"
+
+#define MAX_LINE 60
+#define SERVER_PORT 6666
+
+HttpServer::HttpServer() {
+    std::cout << "this is the constructor " << std::endl;
+}
+
+void HttpServer::Start() {
+    std::cout << "in the start function" << std::endl;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t cliaddr_len;
+    int listend_fd, conn_fd;
+    char buf[MAX_LINE];
+    char str[INET_ADDRSTRLEN];
+    int i, n;
+    listend_fd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    bind(listend_fd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    listen(listend_fd, 20);
+
+    std::cout << "accepting .........." << std::endl;
+
+    while (1) {
+        cliaddr_len = sizeof(client_addr);
+        conn_fd = accept(listend_fd, (struct sockaddr *) &client_addr, &cliaddr_len);
+        n = read(conn_fd, buf, MAX_LINE);
+        std::cout << "recv from port" << SERVER_PORT << std::endl;
+        inet_ntop(AF_INET, &client_addr.sin_addr, str, sizeof(str)),
+                ntohs(client_addr.sin_port);
+        for (int i = 0; i < n; i++) {
+            buf[i] = toupper(buf[i]);
+        }
+        write(conn_fd, buf, n);
+        close(conn_fd);
+    }
+
+}
