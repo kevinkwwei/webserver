fe9b86fdc46cb1bd8fa42d7f44335e042ec0fe9e
kevinkwwei <kevinkwwei@tencent.com>
2023-10-22 23:41:16 +0800

    write data part

--- httpproject/src/http_connection/http_connection.cc
+++ httpproject/src/http_connection/http_connection.cc
@@ -5,15 +5,16 @@
 #include "http_connection.h"
 #include "log/log.h"
 
-const std::string http_200_title = "OK";
-const std::string bad_400_title = "Bad Request";
-const std::string error_400_form = "Your request has bad syntax or is inherently impossible to staisfy.\n";
-const std::string error_403_title = "Forbidden";
-const std::string error_403_form = "You do not have permission to get file form this server.\n";
-const std::string error_404_title = "Not Found";
-const std::string error_404_form = "The requested file was not found on this server.\n";
-const std::string error_500_title = "Internal Error";
-const std::string error_500_form = "There was an unusual problem serving the request file.\n";
+//定义http响应的一些状态信息
+const char *ok_200_title = "OK";
+const char *error_400_title = "Bad Request";
+const char *error_400_form = "Your request has bad syntax or is inherently impossible to staisfy.\n";
+const char *error_403_title = "Forbidden";
+const char *error_403_form = "You do not have permission to get file form this server.\n";
+const char *error_404_title = "Not Found";
+const char *error_404_form = "The requested file was not found on this server.\n";
+const char *error_500_title = "Internal Error";
+const char *error_500_form = "There was an unusual problem serving the request file.\n";
 
 //对文件描述符设置非阻塞
 int setnonblocking(int fd) {
@@ -137,13 +138,99 @@ void http_connection::process() {
     modfd(m_epollfd, m_sockfd, EPOLLOUT);
 }
 
-
+//读取浏览器端发来的全部数据
 bool http_connection::read_once() {
-    return false;
+    if (m_read_idx >= READ_BUFFER_SIZE)
+    {
+        return false;
+    }
+    int bytes_read = 0;
+
+    //LT读取数据
+    if (0 == m_TRIGMode)
+    {
+        bytes_read = recv(m_sockfd, m_read_buf + m_read_idx, READ_BUFFER_SIZE - m_read_idx, 0);
+        m_read_idx += bytes_read;
+
+        if (bytes_read <= 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+        //ET读数据
+    else
+    {
+        // et下一次性读完
+        while (true)
+        {
+            bytes_read = recv(m_sockfd, m_read_buf + m_read_idx, READ_BUFFER_SIZE - m_read_idx, 0);
+            if (bytes_read == -1)
+            {
+                if (errno == EAGAIN || errno == EWOULDBLOCK)
+                    break;
+                return false;
+            }
+            else if (bytes_read == 0)
+            {
+                return false;
+            }
+            m_read_idx += bytes_read;
+        }
+        return true;
+    }
 }
 
+// 像前端发送/写数据
 bool http_connection::write() {
-    return false;
+    int temp = 0;
+    // 要发的数据为空
+    if (bytes_to_send == 0) {
+        // 不写了，变成读，然后都重置
+        modfd(m_epollfd, m_sockfd, EPOLLIN, m_TRIGMode);
+        init();
+        return true;
+    }
+    while (1) {
+        // 写数据
+        temp = writev(m_sockfd, m_iv, m_iv_count);
+        // 发送失败
+        if (temp < 0) {
+            // 如果是缓冲区满了
+            if (errno == EAGAIN) {
+                // 等待下次继续
+                modfd(m_epollfd, m_sockfd, EPOLLOUT, m_TRIGMode);
+                return true;
+            }
+            unmap();
+            return false;
+        }
+        // 发送成功了
+        bytes_have_send += temp;
+        bytes_to_send -= temp;
+        // 发完了第一部分
+        if (bytes_have_send >= m_iv[0].iov_len) {
+            m_iv[0].iov_len = 0;
+            m_iv[1].iov_base = m_file_address + (bytes_have_send - m_write_idx);
+            m_iv[1].iov_len = bytes_to_send;
+        } else {
+            // 第一部分都没发送完，全部重发
+            m_iv[0].iov_base = m_write_buf + bytes_have_send;
+            m_iv[0].iov_len = m_iv[0].iov_len - bytes_have_send;
+        }
+        // 无数据可发了
+        if (bytes_to_send <= 0) {
+            unmap();
+            modfd(m_epollfd, m_sockfd, EPOLLIN, m_TRIGMode);
+            if (m_linger) {
+                init();
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
 
 // 处理接受请求报文
@@ -298,7 +385,120 @@ HTTP_CODE http_connection::parse_content(char *text) {
     return NO_REQUEST;
 }
 
+void http_connection::unmap() {
+    if (m_file_address) {
+        munmap(m_file_address, m_file_stat.st_size);
+        m_file_address = 0;
+    }
+}
+
+// 处理响应报文逻辑
+bool http_connection::process_write(HTTP_CODE ret) {
+    switch (ret) {
+        case HTTP_CODE::BAD_REQUEST:
+            add_status_line(404, error_404_title);
+            add_headers(strlen(error_404_form));
+            if (!add_content(error_404_form))
+                return false;
+            break;
+        case HTTP_CODE::INTERNAL_ERROR:
+            add_status_line(500, error_500_title);
+            add_headers(strlen(error_500_form));
+            if (!add_content(error_500_form))
+                return false;
+            break;
+        case HTTP_CODE::FILE_REQUEST:
+            add_status_line(200, ok_200_title);
+            if (m_file_stat.st_size != 0) {
+                add_headers(m_file_stat.st_size);
+                m_iv[0].iov_base = m_write_buf;
+                m_iv[0].iov_len = m_write_idx;
+                m_iv[1].iov_base = m_file_address;
+                m_iv[1].iov_len = m_file_stat.st_size;
+                m_iv_count = 2;
+                bytes_to_send = m_write_idx + m_file_stat.st_size;
+                return true;
+            } else {
+                const char *ok_string = "<html><body></body></html>";
+                add_headers(strlen(ok_string));
+                if (!add_content(ok_string))
+                    return false;
+            }
+            break;
+        case HTTP_CODE::FORBIDDEN_REQUEST:
+            add_status_line(403, error_403_title);
+            add_headers(strlen(error_403_form));
+            if (!add_content(error_403_form))
+                return false;
+            break;
+        default:
+            return false;
+    }
+    // 为了之后writev使用
+    m_iv[0].iov_base = m_write_buf;
+    m_iv[0].iov_len = m_write_idx;
+    m_iv_count = 1;
+    bytes_to_send = m_write_idx;
+    return true;
+}
+
+// 对于所有add，本质都调用add_response
+bool http_connection::add_response(const char *format, ...) {
+    // 判断现在的写入位置是否还能继续写
+    if (m_write_idx >= WRITE_BUFFER_SIZE)
+        return false;
+    // 保存 ... 的值
+    va_list arg_list;
+    // 初始化arg_list
+    va_start(arg_list, format);
+    auto curr_write_buff = m_write_buf + m_write_idx;
+    auto curr_write_buff_size = WRITE_BUFFER_SIZE - 1 - m_write_idx;
+    // 将arg_list里的可变参数写入curr_write_buff， len大于0且小于curr_write_buff_size证明完全写入
+    int len = vsnprintf(curr_write_buff, curr_write_buff_size, format, arg_list);
+    if (len >= (WRITE_BUFFER_SIZE - 1 - m_write_idx)) {
+        // 清空
+        va_end(arg_list);
+        return false;
+    }
+    // 更新位置
+    m_write_idx += len;
+    va_end(arg_list);
+
+    LOG_INFO("request:%s", m_write_buf);
+
+    return true;
+}
+
 // 处理request，产出response
 HTTP_CODE http_connection::do_request() {
 
+}
+
+bool http_connection::add_headers(int content_length) {
+    return add_content_length(content_length) && add_linger() &&
+           add_blank_line();
+}
+
+bool http_connection::add_blank_line() {
+    return add_response("%s", "\r\n");
+}
+
+bool http_connection::add_content_type() {
+    return add_response("Content-Type:%s\r\n", "text/html");
+}
+
+bool http_connection::add_content_length(int content_length) {
+    return add_response("Content-Length:%d\r\n", content_length);
+}
+
+bool http_connection::add_linger() {
+    return add_response("Connection:%s\r\n", (m_linger == true) ? "keep-alive" : "close");
+}
+
+bool http_connection::add_content(const char *content) {
+    return add_response("%s", content);
+}
+
+bool http_connection::add_status_line(int status, const char *title) {
+    return add_response("%s %d %s\r\n", "HTTP/1.1", status, title);
 }
